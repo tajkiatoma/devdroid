215e6b8e4bc98c64f366127eefbc43760770f400
tajkiatoma <none@none>
2019-09-28 19:08:02 +0600

    loaded top 10 voted posts in last 7 days

--- js/mostVotedPostController.js
+++ js/mostVotedPostController.js
@@ -1,6 +1,11 @@
 myApp.controller('MostVotedPostController', ['$scope', '$http', function($scope, $http) {
     $scope.title = 'Most Voted Posts in Past Week';
-    var url = "https://api.stackexchange.com/2.2/questions?site=stackoverflow&tagged=android&sort=votes&fromdate=1569110400&todate=1569628800&order=desc"
+    var daysToSubtract = 7;
+    var toDate = Math.trunc(Date.now()/1000);
+    var fromDate = toDate - (daysToSubtract * 24 * 60 * 60);
+    console.log(fromDate);
+    console.log(toDate);
+    var url = "https://api.stackexchange.com/2.2/questions?site=stackoverflow&tagged=android&sort=votes&fromdate="+fromDate+"&todate="+toDate+"&order=desc"
     $http.get(url).then(function(response){
         $scope.posts = response.data.items.slice(0, 10);
     });
