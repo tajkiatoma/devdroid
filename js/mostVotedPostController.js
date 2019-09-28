ae312d886e7972d7c5f5d3b6c7d686281ed651c4
tajkiatoma <none@none>
2019-09-28 14:42:43 +0600

    loaded top 10 voted posts in a fixed week

--- js/mostVotedPostController.js
+++ js/mostVotedPostController.js
@@ -1,3 +1,7 @@
-myApp.controller('MostVotedPostController', ['$scope', function($scope) {
+myApp.controller('MostVotedPostController', ['$scope', '$http', function($scope, $http) {
     $scope.title = 'Most Voted Posts in Past Week';
+    var url = "https://api.stackexchange.com/2.2/questions?site=stackoverflow&tagged=android&sort=votes&fromdate=1569110400&todate=1569628800&order=desc"
+    $http.get(url).then(function(response){
+        $scope.posts = response.data.items.slice(0, 10);
+    });
   }]);
