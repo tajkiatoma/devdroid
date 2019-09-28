4d223d2f484a445e5b43d30b81d4a87e7c3f5ad1
tajkiatoma <none@none>
2019-09-28 21:54:13 +0600

    loaded newest questions with design

--- js/newestPostController.js
+++ js/newestPostController.js
@@ -1,5 +1,6 @@
 myApp.controller('NewestPostController', ['$scope', '$http', function($scope, $http) {
     $scope.title = 'Newest Posts';
+    $scope.now = Math.trunc(Date.now()/1000);
     var url = "https://api.stackexchange.com/questions?site=stackoverflow&tagged=android&sort=creation&order=desc";
     $http.get(url).then(function(response){
         $scope.posts = response.data.items.slice(0, 10);
