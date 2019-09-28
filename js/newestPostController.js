cdd3cdec06692b789f66d8562bfd45fad2b7dd95
tajkiatoma <none@none>
2019-09-28 14:39:46 +0600

    loaded top 10 newest posts

--- js/newestPostController.js
+++ js/newestPostController.js
@@ -1,3 +1,7 @@
-myApp.controller('NewestPostController', ['$scope', function($scope) {
-  $scope.title = 'Newest Posts';
+myApp.controller('NewestPostController', ['$scope', '$http', function($scope, $http) {
+    $scope.title = 'Newest Posts';
+    var url = "https://api.stackexchange.com/questions?site=stackoverflow&tagged=android&sort=creation&order=desc";
+    $http.get(url).then(function(response){
+        $scope.posts = response.data.items.slice(0, 10);
+    });
 }]);
