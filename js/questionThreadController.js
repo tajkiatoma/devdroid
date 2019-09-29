fe74c1de8f868558259b93dc1878c6fa1b01a077
tajkiatoma <none@none>
2019-09-29 17:41:25 +0600

    modified question view in question thread

--- js/questionThreadController.js
+++ js/questionThreadController.js
@@ -1,7 +1,9 @@
 myApp.controller('QuestionThreadController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
     var questionUrl = "https://api.stackexchange.com/2.2/questions/"+$routeParams.questionId+"?site=stackoverflow&filter=withbody";
     $http.get(questionUrl).then(function(response){
-        $scope.question = response.data.items[0];
+        var question = response.data.items[0];
+        question.fromNow = moment(question.creation_date*1000).fromNow();
+        $scope.question = question;
     });
     var answerUrl = "https://api.stackexchange.com/2.2/questions/"+$routeParams.questionId+"/answers?site=stackoverflow&sort=votes&order=desc&filter=withbody";
         $http.get(answerUrl).then(function(response){
