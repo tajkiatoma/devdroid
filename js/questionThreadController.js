30d1f53b07387b3c310cda0f669e939dd7f53fba
tajkiatoma <none@none>
2019-09-29 11:41:09 +0600

    loaded question in question thread page

--- js/questionThreadController.js
+++ js/questionThreadController.js
@@ -1,7 +1,10 @@
 myApp.controller('QuestionThreadController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
-    $scope.questionId = $routeParams.questionId;
-    var url = "https://api.stackexchange.com/2.2/questions/"+$routeParams.questionId+"/answers?site=stackoverflow&sort=votes&order=desc&filter=withbody";
-        $http.get(url).then(function(response){
+    var questionUrl = "https://api.stackexchange.com/2.2/questions/"+$routeParams.questionId+"?site=stackoverflow&filter=withbody";
+    $http.get(questionUrl).then(function(response){
+        $scope.question = response.data.items[0];
+    });
+    var answerUrl = "https://api.stackexchange.com/2.2/questions/"+$routeParams.questionId+"/answers?site=stackoverflow&sort=votes&order=desc&filter=withbody";
+        $http.get(answerUrl).then(function(response){
             $scope.answers = response.data.items;
         });
 }]);
